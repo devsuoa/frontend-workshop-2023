2617bf6d015e40289776c267a90969dc6dca1e29
KeiHara <84754523+KeiHara@users.noreply.github.com>
2023-05-16 22:19:46 +1200

    making start template

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
-import MainPage from './Components/MainPage/MainPage';
 
 function App() {
 
-  // Initialise useState hook for storing and updating new notes
-  const [allNotes, setAllNotes] = useState([]);
-
   return (
     <div className="App">
-      <MainPage allNotes={allNotes} setAllNotes={setAllNotes} />
+      app
     </div>
   );
 }
